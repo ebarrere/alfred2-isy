4383fd34e90f452ad2aa4617d9d9dfc7592cac83
Elliott Barrere  <elliott@continuum.us>
2013-12-24 14:02:41 -0800

    add support for scenes (groups)

--- workflow/isy_control.rb
+++ workflow/isy_control.rb
@@ -5,31 +5,27 @@ $: << File.expand_path(File.dirname(__FILE__))
 require 'open-uri'
 require "bundle/bundler/isy"
 require "alfred"
+require "ruhue"
+require "color"
 
 
 Alfred.with_friendly_error do |alfred|
   alfred.with_rescue_feedback = true
   fb = alfred.feedback
+  @debug = true
+  @logger = Logger.new(File.expand_path("~/Library/Logs/Alfred-Workflow.log"))
 
   # puts "#{ARGV.class}"
   node_name, node_address, value = ARGV[0].split(':') # ARGV is a one-element array containing a :-delimited string
-  if value != "group"
-    value = value.to_i
-  else
-    group = true
-  end
+  # if value != "group"
+  value = value.to_i
+  # else
+    # group = true
+  # end
   node_address = URI::encode(node_address)
-  if group
-    members = ISY.get("/rest/nodes/#{node_address}?members=true")['nodeInfo']['group']['members']
-    members.each do |member|
-      
-    url = "/rest/nodes/#{node_address}/cmd/DON"
-    action = value == 0 ? "on" : "off"
-  else
-    url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DON" : "/rest/nodes/#{node_address}/cmd/DOF"
-    action = value == 0 ? "on" : "off"
-  end
-  puts url
+  url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DON" : "/rest/nodes/#{node_address}/cmd/DOF"
+  @logger.debug("url: #{url}") if @debug
+  action = value == 0 ? "on" : "off"
   response = ISY.get(url)
   # puts response["RestResponse"]["succeeded"]
   unless response["RestResponse"]["succeeded"]
@@ -37,5 +33,18 @@ Alfred.with_friendly_error do |alfred|
     print "Call to #{url} failed!"
   else
     print "#{node_name} turned #{action}"
+  end
+
+  if node_name =~ /Hue lamps/ # set holiday color on hue lamps
+    client = Ruhue::Client.new(Ruhue::discover, 'elliottbarrere')
+    light1 = client.light(1)
+    light2 = client.light(2)
+    light3 = client.light(3)
+    # light2.lselect
+    # light2.hsl = Color::RGB::Orchid.to_hsl
+    # light2.select
+    # light2.hue=(39682)
+    light1.hue=(0)
+    light2.hue=(0)
   end
 end
