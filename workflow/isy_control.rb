26793e6497d2f11e30e4320ed1c1c5473aa3376f
Elliott Barrere  <elliott@continuum.us>
2014-10-27 12:56:42 -0700

    Update isy_{query,control} to use the new class structure

--- workflow/isy_control.rb
+++ workflow/isy_control.rb
@@ -2,54 +2,74 @@
 # encoding: utf-8
 
 $: << File.expand_path(File.dirname(__FILE__))
+require 'lib/isy'
+require 'alfred'
 require 'open-uri'
-require "isy"
-require "alfred"
-require "ruhue"
-require "color"
+require 'ruhue'
+require 'color'
 
 
 Alfred.with_friendly_error do |alfred|
   alfred.with_rescue_feedback = true
   fb = alfred.feedback
-  @debug = true
-  @logger = Logger.new(File.expand_path("~/Library/Logs/Alfred-Workflow.log"))
+  log_file = File.new(File.expand_path("~/Library/Logs/Alfred-Workflow.log"),'a')
+  logger = Logger.new(log_file)
+  $stderr = log_file
+  debug = true
 
-  # puts "#{ARGV.class}"
-  node_name, node_address, value = ARGV[0].split(':') # ARGV is a one-element array containing a :-delimited string
+  # logger.debug("#{ARGV}") if debug
+  node_name, node_address, node_level, value = ARGV[0].split(':') # ARGV is a one-element array containing a :-delimited string
   # if value != "group"
-  value = value.to_i
-  @logger.debug("value: #{value}") if @debug
+  # value = value.to_i
+  logger.debug("node_name: \"#{node_name}\", node_address: \"#{node_address}\", node_level: \"#{node_level}\", value: \"#{value}\"") if debug
   # else
     # group = true
   # end
-  node_address = URI::encode(node_address)
-  url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DOF" : "/rest/nodes/#{node_address}/cmd/DON/#{value}"
-  # url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DON/#{value}" : "/rest/nodes/#{node_address}/cmd/DOF"
-  @logger.debug("url: #{url}") if @debug
-  action = value == 0 ? "on" : "off"
-  response = ISY.get(url)
-  # puts response["RestResponse"]["succeeded"]
-  unless response["RestResponse"]["succeeded"]
-    response = ISY.get(/rest/)
-    print "Call to #{url} failed!"
-  else
-    print "#{node_name} turned #{action}"
-  end
 
-  if node_name =~ /Hue lamps/ # set random color on hue lamps
-    sleep(1)
-    client = Ruhue::Client.new(Ruhue::discover, 'elliottbarrere')
-    light1 = client.light(1)
-    light2 = client.light(2)
-    light3 = client.light(3)
-    # light2.lselect
-    # light2.hsl = Color::RGB::Orchid.to_hsl
-    # light2.select
-    # light2.hue=(39682)
-    light1.hue=(rand(2**16-1))
-    light2.hue=(rand(2**16-1))
-    light3.on
-    light3.hue=(rand(2**16-1))
+  isy = ISY.new('https://t.0o0.bz','admin','password')
+
+  # TODO: GROUPS SHOULD ALWAYS HIT DON
+  case value
+  when 'on'
+    logger.debug("Turning #{node_address} on.") if debug
+    logger.debug(isy.node_on(node_address))
+  when /^(off|0)$/
+    logger.debug("Turning #{node_address} off.") if debug
+    logger.debug(isy.node_off(node_address))
+  when /^[0-9]+$/
+    logger.debug("Setting #{node_address} to #{value}.") if debug
+    logger.debug(isy.set_node_level(node_address,value))
+  else
+    logger.debug("#{node_address} toggled") if debug
+    logger.debug(isy.node_on?(node_address) ? isy.node_off(node_address) : isy.node_on(node_address))
   end
+  # node_address = URI::encode(node_address)
+  # url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DOF" : "/rest/nodes/#{node_address}/cmd/DON/#{value}"
+  # # url = value == 0 ? "/rest/nodes/#{node_address}/cmd/DON/#{value}" : "/rest/nodes/#{node_address}/cmd/DOF"
+  # logger.debug("url: #{url}") if debug
+  # action = value == 0 ? "on" : "off"
+  # response = ISY.get(url)
+  # # puts response["RestResponse"]["succeeded"]
+  # unless response["RestResponse"]["succeeded"]
+  #   response = ISY.get(/rest/)
+  #   print "Call to #{url} failed!"
+  # else
+  #   print "#{node_name} turned #{action}"
+  # end
+
+  # if node_name =~ /Hue lamps/ # set random color on hue lamps
+  #   sleep(1)
+  #   client = Ruhue::Client.new(Ruhue::discover, 'elliottbarrere')
+  #   light1 = client.light(1)
+  #   light2 = client.light(2)
+  #   light3 = client.light(3)
+  #   # light2.lselect
+  #   # light2.hsl = Color::RGB::Orchid.to_hsl
+  #   # light2.select
+  #   # light2.hue=(39682)
+  #   light1.hue=(rand(2**16-1))
+  #   light2.hue=(rand(2**16-1))
+  #   light3.on
+  #   light3.hue=(rand(2**16-1))
+  # end
 end
--- workflow/isy_query.rb
+++ workflow/isy_query.rb
@@ -2,134 +2,115 @@
 # encoding: utf-8
 
 $: << File.expand_path(File.dirname(__FILE__))
+require 'lib/isy'
+
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require 'open-uri'
-require "bundle/bundler/setup"
-require "isy"
-require "alfred"
+require 'bundle/bundler/setup'
+require 'alfred'
 
 def faux_query(query)
   (query && query.length > 0) ? query : "..."
 end
 
-def check_node(type,query,address)
-  q_string = %Q!#{type}["name"]!
-  node_address = address
-  if @new_value
-    value = @new_value
-  elsif query =~ /#{q_string}/
-    node_address = URI::encode(group["address"])
-    @new_value = ISY.get("/rest/nodes/#{node_address}")["nodeInfo"]["properties"]["property"][0]["value"]
-    cur_value = ISY.get("/rest/nodes/#{node_address}")["nodeInfo"]["properties"]["property"][2]["value"]
-    if @new_value == cur_value
-      value = 0
-    else
-      value = @new_value
-    end
-  end
-end
-
 Alfred.with_friendly_error do |alfred|
-  alfred.with_rescue_feedback = true
-  fb = alfred.feedback
-
   # set up logging
-  @logger = Logger.new(File.expand_path("~/Library/Logs/Alfred-Workflow.log"))
-  @debug = false
+  log_file = File.new(File.expand_path("~/Library/Logs/Alfred-Workflow.log"),'a')
+  logger = Logger.new(log_file)
+  $stderr = log_file
+  debug = false
 
-  # set variables and make call to ISY
-  query = faux_query(ARGV.join(' '))
-  query = query.split(':').first
-  @new_value = faux_query(ARGV.join(' '))
-  @new_value = @new_value.split(':')[1]
-  @new_value = @new_value.to_i if @new_value
-
-  response = ISY.get("/rest/nodes/")
-  unless response.class == HTTParty::Response
-    raise Alfred::NoBundleIDError, "Got unexpected response class \"#{response.class}\" from ISY!  Check your URL."
-  end
-  nodes = response['nodes']['node'].sort_by! { |hash| hash['name']}
-  groups = response['nodes']['group'].sort_by! { |hash| hash['name']}.reverse
-  unless nodes.is_a? Array
-    raise Alfred::NoBundleIDError, "Got wrong reply type from ISY!  Check your URL."
-  end
-  unless nodes[1].has_key? 'name' and nodes[1]["name"].is_a? String
-    raise Alfred::NoBundleIDError, "ISY output is an unexpected type: #{nodes[1].class}"
+  # configure alfred module
+  alfred.with_rescue_feedback = true
+  alfred.with_cached_feedback do
+    # expire in 1 hour
+    use_cache_file :expire => 3600
+    # use_cache_file :file => "/tmp/alfred2-isy_cache_file", :expire => 3600
   end
 
-  # handle nodes (single lights/non-scenes)
-  nodes.each do |node|
-    next if node["type"] =~ /3.7.74.0|0.18.0.0/ # hack to remove known "scenes" from node-list
-    value = check_node(node,query,node["address"])
-    # if @new_value
-    #   value = @new_value
-    # elsif query =~ /#{node["name"]}/
-    #   node_address = URI::encode(node["address"])
-    #   @new_value = ISY.get("/rest/nodes/#{node_address}")["nodeInfo"]["properties"]["property"][0]["value"]
-    #   cur_value = ISY.get("/rest/nodes/#{node_address}")["nodeInfo"]["properties"]["property"][2]["value"]
-    #   if @new_value == cur_value
-    #     value = 0
-    #   else
-    #     value = @new_value
-    #   end
-    # end
-    # add the item to Alfred Feedback
-    fb.add_item({
-      :title        => node["name"],
-      :subtitle     => "address: #{node["address"]}",
-      :autocomplete => "#{node["name"]}:",
-      :arg          => "#{node["name"]}:#{node["address"]}:#{value}:#{@new_value}",
-      :valid        => "yes",
-    })
-  end
+  # set program variables from Alfred's query string
+  (query, value) = faux_query(ARGV.join(' ')).split(':')
 
-  # handle scenes, or "groups" in ISY-speak
-  groups.each do |group|
-
-    # first we calculate "on-ness" of the group
-    # this is an average of the values of each group member
-    sum = 0
-    group["members"].each do |member|
-      if group["name"] =~ / - / # skip e.g. "Stereo - Power" and "Remote - A"
-        @avg_value = 255
-        @logger.debug("Average value of \"#{group['name']}\": #{@avg_value} (forced)") if @debug
-        next
+  # Test if cache exists and is valid
+  if fb = alfred.feedback.get_cached_feedback # cached feedback is valid
+    # Make sure our cached items have the correct ARG value!
+    # logger.debug("fb: #{fb.methods}")
+    if value
+      fb.items.each do |item|
+        item.arg = item.arg.sub(/[^:]*$/,value)
       end
+    end
+    puts fb.to_alfred(query)
+  else # cached feedback not valid or nonexistent
+    fb = alfred.feedback
+    # fb.add_item({:title => 'waiting'})
+    puts fb.to_alfred(query)
+    isy = ISY.new('https://t.0o0.bz','admin','password')
 
-      # create an array of nodes in the group
-      node_ids = Array.new
-      member[1].each do |node_id|
-        if node_id.is_a?(Hash)
-          node_ids << node_id["__content__"]
-        end
-      end
-
-      # loop through all known nodes, checking if it is a member of our group
-      # if so, add its current value to our sum
-      nodes.each do |node|
-        next unless node.has_key? 'address'
-        next if node["name"] =~ / - / # skip e.g. "Stereo - Power" and "Remote - A"
-        if node_ids.include? node['address']
-          @logger.debug("\"#{group['name']}\" total (#{sum}) += (#{node['property']['value']}) from \"#{node['name']}\"") if @debug
-          sum += node['property']['value'].to_i
-        end
-      end
-
-      # calculate the average "on-ness"
-      # @logger.debug("node_ids: #{node_ids}")
-      node_ids.length > 0 && @avg_value = sum/node_ids.length
-      @logger.debug("Average value of \"#{group['name']}\": #{@avg_value} (#{sum}/#{node_ids.length})") if @debug
+    # handle nodes (single lights/non-scenes)
+    isy.nodes.sort_by! { |hash| hash['name']}.each do |node|
+      next if node["type"] =~ /3.7.74.0|0.18.0.0/ # hack to remove known "scenes" from node-list
+      node_level = isy.node_level(node['address'])
+      # add the item to Alfred Feedback
+      fb.add_item({
+        :title        => node['name'],
+        :subtitle     => "address: #{node['address']}",
+        :autocomplete => "#{node['name']}:",
+        :arg          => "#{node['name']}:#{node['address']}:#{node_level}:#{value}",
+        :valid        => "yes",
+      })
     end
 
-    # add the item to Alfred Feedback
-    fb.add_item({
-      :title        => group["name"],
-      :subtitle     => "address: #{group["address"]}",
-      # :subtitle     => st,
-      :autocomplete => "#{group["name"]}:",
-      :arg          => "#{group["name"]}:#{group["address"]}:#{@avg_value}:#{@new_value}",
-      :valid        => "yes",
-    })    
+    # handle scenes, or "groups" in ISY-speak
+    isy.groups.sort_by! { |hash| hash['name']}.reverse.each do |group|
+
+      # first we calculate "on-ness" of the group
+      # this is an average of the values of each group member
+      # sum = 0
+      # group["members"].each do |member|
+      #   if group["name"] =~ / - / # skip e.g. "Stereo - Power" and "Remote - A"
+      #     @avg_value = 255
+      #     logger.debug("Average value of \"#{group['name']}\": #{@avg_value} (forced)") if debug
+      #     next
+      #   end
+
+      #   # create an array of nodes in the group
+      #   node_ids = Array.new
+      #   member[1].each do |node_id|
+      #     if node_id.is_a?(Hash)
+      #       node_ids << node_id["__content__"]
+      #     end
+      #   end
+
+      #   # loop through all known nodes, checking if it is a member of our group
+      #   # if so, add its current value to our sum
+      #   isy.nodes.each do |node|
+      #     next unless node.has_key? 'address'
+      #     next if node["name"] =~ / - / # skip e.g. "Stereo - Power" and "Remote - A"
+      #     if node_ids.include? node['address']
+      #       logger.debug("\"#{group['name']}\" total (#{sum}) += (#{node['property']['value']}) from \"#{node['name']}\"") if debug
+      #       sum += node['property']['value'].to_i
+      #     end
+      #   end
+
+      #   # calculate the average "on-ness"
+      #   # logger.debug("node_ids: #{node_ids}")
+      #   node_ids.length > 0 && @avg_value = sum/node_ids.length
+      #   logger.debug("Average value of \"#{group['name']}\": #{@avg_value} (#{sum}/#{node_ids.length})") if debug
+      # end
+
+      # add the item to Alfred Feedback
+      fb.add_item({
+        :title        => group["name"],
+        :subtitle     => "address: #{group["address"]}",
+        # :subtitle     => st,
+        :autocomplete => "#{group["name"]}:",
+        :arg          => "#{group["name"]}:#{group["address"]}:0:#{value}",
+        :valid        => "yes",
+      })    
+    end
+    # puts fb.to_xml(query)
+    fb.put_cached_feedback
+    puts fb.to_alfred
   end
-  puts fb.to_xml(query)
+
 end
